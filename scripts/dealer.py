e7a87b6f003825e73acf82064e45f199504e50db
TonoLeo <s20c1078un@s.chibakoudai.jp>
2021-12-25 18:13:16 +0900

    pubの追加

--- scripts/dealer.py
+++ scripts/dealer.py
@@ -1,10 +1,18 @@
 #!/usr/bin/env python3
 
-#import rospy
-#from std_msgs.msg import Int32
+import rospy
+from std_msgs.msg import Int32
 import random
 
+rospy.init_node('dice')
+pub = rospy.Publisher('dealer', Int32, queue_size=1)
 x = random.randint(1,6)
+y = random.randint(1,6)
+sum = x + y
+
 print(x)
+print(y)
+print(sum)
 
 #rospy.init_node('dice')
+pub.publish(sum)
